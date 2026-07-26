256ccbd5a3b27377a76245707cb101f799108d26
pratheeknathani <181894361+pratheeknathani@users.noreply.github.com>
2026-07-25 19:59:33 -0700

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        INF = 2147483647
+        #multi bfs
+        q = deque()
+        visited = set()
+
+        def addCell(r, c):
+            if(r<0 or r== ROWS or c<0 or c==COLS or grid[r][c]==-1 or (r,c) in visited):
+                return
+            q.append((r,c))
+            visited.add((r,c))
+
+        #we want to run bfs from the treasure chests
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                   q.append((r,c))
+                   visited.add((r,c))
+        
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                grid[r][c] = dist
+                addCell(r-1, c)
+                addCell(r+1, c)
+                addCell(r, c-1)
+                addCell(r, c+1)
+            dist +=1
